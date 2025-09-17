88d4eb5863867cd1e85ffa5bdb9521fa8f295948
vandalay-Abhishek <abhishek.kv@vandalayglobal.com>
2025-09-17 22:26:17 +0530

    Calculator  reitration as command line app with out waiting for user input

--- main.c
+++ main.c
@@ -1,60 +1,24 @@
 #include<stdio.h>
-#include<stdlib.h>
 
-int calculate(int operand[2],char operator);
-int add(int a,int b);
-int substract(int a,int b);
-int multiply(int a,int b);
-int dvision(int a,int b);
+int main(int argcount,char *argvalues[]){
+	/*
+	 * This is calcualter reitration want to add 
+	 *   time calculator (time conversion,time calculation)
+	 *   basic calculation fetures 
+	 *   trignometric calculation
+	 *   finance calculation 
+	 *   avreage calculation
+	 *
+	 * basic synatx:
+	 *     calcualtor 10+22+10+22
+	 *     calculator [option] [oprand and operator]
+	 *     calculator [option as oprator] oprands 
+	 *
+	  */
 
-int main(){
-    int operand[2],result;
-    char operator; 
-    printf("\nEnter numbers: ");
-    scanf("%d",&operand[0]);
-    scanf("%d",&operand[1]);
-
-    printf("\nEnter the operand: ");
-    scanf(" %c",&operator);
-
-    printf("\n");
-
-    result=calculate(operand,operator);
-
-    printf("Calcualtion Result is:%d \n", result);
-    return 0;
+	printf("argument count: %d\n",argcount); 
+	printf("\n argument values:\n************************\n"); 
+	for (int i=0;i<argcount;i++){
+		printf("[%d] = %s\n",i, argvalues[i]);
+	}
 }
-
-int calculate(int opreand[2],char operator){
-    switch(operator){
-        case '+':
-            return add(opreand[0],opreand[1]);
-        case '-':
-            return substract(opreand[0],opreand[1]);
-        case '*':
-            return multiply(opreand[0],opreand[1]);
-        case 'x':
-            return multiply(opreand[0],opreand[1]);
-        case '/':
-            return dvision(opreand[0],opreand[1]);
-        default:  
-            printf("Operation not found!!!!\n");
-            exit(1);
-    }
-}
-
-
-int add(int a,int b){
-    return a+b;
-}
-int substract(int a,int b){
-    return a-b;
-}
-int multiply(int a,int b){
-    return a*b;
-}
-
-int dvision(int a,int b){
-    return a/b;
-}
-
